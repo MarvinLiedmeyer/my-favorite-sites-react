69995b4561a3901370f5c909a2880647d9bd1df8
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-08-22 17:07:15 +0200

    :recycle: Refactor code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,8 @@ import { hot } from 'react-hot-loader';
 import Headline from './header/headline/Headline';
 import Intro from './header/intro/Intro';
 import CreateList from './top_sites/top_sites';
-import Formular from './formular/formular'
+import Formular from './formular/formular';
 
-// We use PureComponent instead of Component because it handles the shouldComponentUpdate method for us.
-// If we want to define our own shouldComponentUpdate logic we have to use Component instead of PureComponent.
 class App extends PureComponent {
     render() {
         return (
--- src/components/top_sites/top_sites.jsx
+++ src/components/top_sites/top_sites.jsx
@@ -1,65 +1,65 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { PureComponent } from 'react';
-import { Accordion } from 'chayns-components';
-import { List, ListItem, SmallWaitCursor } from 'chayns-components';
-
+import { Accordion, List, ListItem } from 'chayns-components';
 
 class CreateList extends PureComponent {
     constructor() {
-        super()
+        super();
         this.state = {
-            loading: false,
             topSitesList: [],
-            placeholderList: [],
-        }
+        };
     }
 
     componentDidMount() {
-        this.setState({ loading: true });
-        fetch("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50")
+        this.setState();
+        fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
             .then(response => response.json())
-            .then(data => {
+            .then((data) => {
                 this.setState({
-                    loading: false,
                     topSitesList: data.Data,
-                })
+                });
                 this.list = data.Data;
-            })
+            });
     }
 
     createList() {
         const { topSitesList } = this.state;
-        return topSitesList.map(({ siteId, locationId, appstoreName, facebookId }) => <ListItem
-            title={appstoreName} onClick={() => window.open(`https://chayns.net/${siteId}`)}
-            image={`https://sub60.tobit.com/l/${siteId}`}
-            right={(
-                <img
-                    style={{
-                        margin: "5px",
-                        width: "20px",
-                        height: "20px"
-                    }}
-                    src='https://de.facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png'
-                    onClick={() => window.open(`https://www.facebook.com/profile.php?id=${facebookId}`)}
+        return topSitesList.map(({
+            siteId, appstoreName, facebookId
+        }) => (
+                <ListItem
+                    title={appstoreName}
+                    onClick={() => window.open(`https://chayns.net/${siteId}`)}
+                    image={`https://sub60.tobit.com/l/${siteId}`}
+                    right={(
+                        <img
+                            alt="Das ist ein Facebook-Icon!"
+                            style={{
+                                margin: '5px',
+                                width: '20px',
+                                height: '20px'
+                            }}
+                            src="https://de.facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png"
+                            onClick={() => window.open(`https://www.facebook.com/profile.php?id=${facebookId}`)}
+                        />
+                    )}
                 />
-            )}
-        />
-        );
+            ));
     }
 
     render() {
         const siteList = this.createList();
 
         const onSearch = (searchText) => {
-            const { topSitesList } = this.state;
             const filterList = this.list.filter(e => e.appstoreName.toLowerCase().includes(searchText.toLowerCase()));
             this.setState({
                 topSitesList: null
-            })
+            });
             this.setState({
                 topSitesList: filterList
             });
             return (this.createList());
-        }
+        };
 
         return (
             <Accordion head="Top Seiten" onSearchEnter={onSearch} searchPlaceholder="Suchen...">
@@ -69,9 +69,8 @@ class CreateList extends PureComponent {
                     </List>
                 </div>
             </Accordion >
-        )
+        );
     }
-
 }
 
 export default CreateList;
