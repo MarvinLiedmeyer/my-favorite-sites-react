b5032fb68a1ff32fde1c7743c41e1d71d547831f
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-08-22 16:57:33 +0200

    :sparkles: Add validation

--- src/components/formular/addMessage.jsx
+++ src/components/formular/addMessage.jsx
@@ -1,14 +1,8 @@
-const addMessage = async ({ id, name, email, address, comment }) => {
-
+const addMessage = async ({
+    id, name, email, address, comment
+}) => {
     await chayns.login();
-    // if (!emailIsValid(email)) {
-    //     chayns.dialog.alert('', 'Ungültige E-mail-Eingabe');
-    //     return false;
-    // }
-    // if (!uRLIsValid(address)) {
-    //     chayns.dialog.alert('', 'Ungültige Internetadress-Eingabe');
-    //     return false;
-    // }
+
     await chayns.intercom.sendMessageToPage({
         text: `${'Neue Seite'}
                         ${'ID: '} ${id}
@@ -19,6 +13,6 @@ const addMessage = async ({ id, name, email, address, comment }) => {
     });
     chayns.dialog.alert('', 'Danke fürs hinzufügen deiner Webseite. Deine Anfrage wird bearbeitet!');
     return true;
-}
+};
 
 export default addMessage;
